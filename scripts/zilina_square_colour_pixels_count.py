25da76b544a1cd9318978e6bba4aee29e7970b93
Ghost <michalmulik1@gmail.com>
2020-04-13 15:49:44 +0200

    new features, code changed slightly - tested

--- scripts/zilina_square_colour_pixels_count.py
+++ scripts/zilina_square_colour_pixels_count.py
@@ -1,48 +1,90 @@
 from PIL import Image
 import numpy as np
-
-XofTopLeft = 0
-YofTopLeft = 0
-
-XofBotRight = 1820
-YofBotRight = 1624
-
-startingPosition = 2
-colourRange = 78
-RGB = 3
+import math
 
 mapOfZilina = Image.open("mapa_okrsky.png").convert('RGB')
 pix = mapOfZilina.load()
 
+w, h = mapOfZilina.size  # getting max. size of both axis
 
-data = np.genfromtxt('color-coding-okrsky.dat',
-                     dtype=None,
-                     usecols=(1, 2, 3),
-                     delimiter=' ')
+XofTopLeft = 0
+YofTopLeft = 0
 
-#print(data)
-#print(mapOfZilina)
+XofBotRight = w
+YofBotRight = h
 
-IDs = []
-newIDs = []
-newPixelColours = []
+startingPosition = 2  # starting position of ID in a file
+colourRange = 78  # colour range from the file
+RGB = 3  # number of main colours in RGB model
+
+numberOfSquaresOnMap = 9  # has to be squared number for integer number
+numberOfSquaresInRow = int(math.sqrt(numberOfSquaresOnMap))  # number of squares in a row/column
+
+xLineOfSquare = int((XofBotRight - XofTopLeft) / numberOfSquaresInRow)  # x difference from square to square
+yLineOfSquare = int((YofBotRight - YofTopLeft) / numberOfSquaresInRow)  # y difference from square to square
+
+colors = np.genfromtxt('color-coding-okrsky.dat',
+                       dtype=None,
+                       usecols=(1, 2, 3),
+                       delimiter=' ')
+
+inhabitants = np.genfromtxt('number_inhabitants_okrsky.dat',
+                            dtype=None,
+                            usecols=[1],
+                            delimiter=' ')
+
+allPixelsOfColour = np.genfromtxt('number-pixels-in-okrsky.dat',
+                                  dtype=None,
+                                  usecols=[1],
+                                  delimiter='   ')
+
+# Printing information about objects{files
+# print(mapOfZilina)
+# print('\n', inhabitants)
+# print('\n', allPixelsOfColour)
+
+TotalIDsPixelCount = []  # total pixels of different colour ID on one big square
+totalSumOfInhabitantsInMap = 0  # total inhabitants in whole one big square
 
 for i in range(0, colourRange):
-    IDs.append(0)
+    TotalIDsPixelCount.append(0)
 
-for i in range(XofTopLeft, XofBotRight):
-    for j in range(YofTopLeft, YofBotRight):
-        for k in range(0, colourRange):
-            theSame = 1
-            for m in range(RGB):
-                if data[k][m] != pix[i, j][m]:
-                    theSame = 0
-                    break
-            if theSame:
-                IDs[k] += 1
+for h in range(numberOfSquaresInRow):  # each square in a row
+    for g in range(numberOfSquaresInRow):  # each square in column
 
-print('\n', "Count of pixels by colour ID :", '\n')
+        IDsPixelCount = []  # each square pixels of different colour ID
+
+        for i in range(0, colourRange):
+            IDsPixelCount.append(0)
+
+        for i in range(XofTopLeft + h * xLineOfSquare, h * xLineOfSquare + xLineOfSquare):
+            for j in range(YofTopLeft + g * yLineOfSquare, g * yLineOfSquare + yLineOfSquare):
+                for k in range(0, colourRange):
+                    theSame = 1
+                    for m in range(RGB):
+                        if colors[k][m] != pix[i, j][m]:
+                            theSame = 0
+                            break
+                    if theSame:
+                        IDsPixelCount[k] += 1
+                        TotalIDsPixelCount[k] += 1
+
+        print('\n', "Count of pixels by colour ID :", '\n')
+        for i in range(0, colourRange):
+            print(i + startingPosition, " ", IDsPixelCount[i])
+
+        inhabitantsInSquare = []  # array of inhabitants in small square
+        sumOfInhabitantsInSquare = 0  # sum of all inhabitants in small square
+
+        for i in range(0, colourRange):
+            inhabitantsInSquare.append(IDsPixelCount[i] / allPixelsOfColour[i] * inhabitants[i])
+            sumOfInhabitantsInSquare += inhabitantsInSquare[i]
+
+        totalSumOfInhabitantsInMap += sumOfInhabitantsInSquare
+
+        print('\n', "Number of inhabitants in small square is : ", int(sumOfInhabitantsInSquare))
+
 for i in range(0, colourRange):
-    print(i+startingPosition, " ", IDs[i])
+    print(i + startingPosition, " ", TotalIDsPixelCount[i])
 
-
+print('\n', "Number of inhabitants on big square is : ", int(totalSumOfInhabitantsInMap))
