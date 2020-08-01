43bc1dcff587e29aab40846ae39cc88d254a4b59
Ghost <michalmulik1@gmail.com>
2020-08-01 13:39:32 +0200

    Making more space in code

--- scripts/task1.py
+++ scripts/task1.py
@@ -21,17 +21,22 @@ print("Matrix created.")
 
 #mapa.print_OD_matrix_by_squares()
 #print("Matrix printed.")
-
 #mapa.detect_zeros_collumns_and_rows()
+
 print("x")
+
 #mapa.whole_detection()
 mapa.fixOD()
 mapa.fixOD_workday()
+
 print("x")
+
 #mapa.draw_grid(85)
 #mapa.show_map()
+
 # Need to go after population manipulation
 mapa.remove_squares_without_population()
+
 #print("Squares with no humans removed.")
 #mapa.remove_zeros_from_OD()
 #print("Example")
@@ -40,28 +45,37 @@ mapa.remove_squares_without_population()
 #mapa.print_OD_matrix_by_squares_Adam()
 #print("matrix_workday")
 #mapa.print_OD_workday_matrix_by_squares_Adam()
+
 day = 0
 for i in range(len(mapa.squares)):
     mapa.squares[i].inicialize()
+
 mapa.squares[28].change_InfA(50.0)
+
 while day < 150:
     for i in range(0, 77):
         mapa.squares[i].update(i, mapa.OD, mapa.squares)
+
     #mapa.draw_map_with_stops()
     #mapa.draw_grid(85)
-    #mapa.color_square_strategy_square_max_population()
+    #mapa.color_square_strategy_square_max_population() ---------------------------- TOTO ODKOMENTUJ
     #mapa.save_map(day)
+
     day = day + 1
 
 for i in range(len(mapa.squares)):
     #print(i)
+
     mapa.squares[i].print()
+
     #mapa.squares[i].selfplot()
     #zafarbenie mapy podla udajov
     #mapa.color_square_strategy_square_max_population()
     #mapa.show_map()
+
 #mapa.print_squares_with_stops_and_humans()
 #mapa.pomocna()
+
 #print("Advanced matrix printed.")
 #print("Number of squares with humans and with stops :", mapa.get_count_of_squares_with_humans_with_stops())
 #print("Number of squares without humans :", mapa.get_count_of_squares_without_humans())
